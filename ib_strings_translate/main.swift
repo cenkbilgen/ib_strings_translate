339e8d059e9899ab0cfeca2b006b45ee65aa8b33
Cenk Bilgen <cbilgen@gadventures.com>
2018-12-17 19:28:35 -0500

    Initial Commit

---
+++ ib_strings_translate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ib_strings_translate
+//
+//  Created by abc on 2018-12-17.
+//  Copyright © 2018 abc. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
